2c38836880b984c262f71917e52c240c0c27d26a
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-08-01 23:45:10 +0200

    Implemented all filters

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,12 @@ fn run(args: &[String]) -> Result<(), Box<Error>> {
             // println!("Scanlines:\n{:?}", scanlines);
             display_filters(&scanlines);
             let img = unfilter(&ihdr_data, scanlines);
-            println!("{:?}", img.get(393, 161));
+            println!("{:?}", img.get(77, 21));
+            println!("{:?}", img.get(78, 21));
+            println!("{:?}", img.get(79, 21));
+            println!("{:?}", img.get(80, 21));
+            println!("{:?}", img.get(81, 21));
+            println!("{:?}", img.get(82, 21));
             // println!("{:?}", &img.data.as_slice()[0..10]);
             png_valid.iter().for_each(|chunk| {
                 match parse_chunk_data(chunk) {
@@ -446,7 +451,8 @@ fn get_scanlines<'a>(ihdr: &IHDRData, image_data: &'a [u8]) -> Vec<(Filter, &'a 
 fn display_filters(scanlines: &[(Filter, &[u8])]) -> () {
     scanlines
         .iter()
-        .for_each(|(filter, _)| print!("{:?}, ", filter));
+        .enumerate()
+        .for_each(|(i, (filter, _))| print!("{} {:?}, ", i, filter));
     println!("");
 }
 
@@ -465,15 +471,16 @@ fn unfilter(ihdr: &IHDRData, scanlines: Vec<(Filter, &[u8])>) -> Img {
     println!("bytes_per_pixel: {}", bpp);
     // let mut data = Vec::with_capacity(bpp * width * height);
     let mut data = vec![0; bpp * width * height];
-    let previous_slice_start = -(bpp as isize * width as isize);
-    scanlines.iter().fold(
-        previous_slice_start,
-        |previous_slice_start, (filter, line)| match filter {
-            Filter::None => unfilter_none(line, previous_slice_start, &mut data),
-            Filter::Sub => unfilter_sub(bpp, line, previous_slice_start, &mut data),
-            _ => unimplemented!(),
-        },
-    );
+    let line_start = 0;
+    scanlines
+        .iter()
+        .fold(line_start, |line_start, (filter, line)| match filter {
+            Filter::None => unfilter_none(line, line_start, &mut data),
+            Filter::Sub => unfilter_sub(bpp, line, line_start, &mut data),
+            Filter::Up => unfilter_up(line, line_start, &mut data),
+            Filter::Average => unfilter_average(bpp, line, line_start, &mut data),
+            Filter::Paeth => unfilter_paeth(bpp, line, line_start, &mut data),
+        });
     assert_eq!(height, scanlines.len());
     assert_eq!(data.len(), bpp * width * height);
     Img {
@@ -485,14 +492,113 @@ fn unfilter(ihdr: &IHDRData, scanlines: Vec<(Filter, &[u8])>) -> Img {
     }
 }
 
-fn unfilter_sub(bpp: usize, line: &[u8], previous_slice_start: isize, data: &mut Vec<u8>) -> isize {
-    let line_start = (previous_slice_start + line.len() as isize) as usize;
+fn unfilter_none(line: &[u8], line_start: usize, data: &mut Vec<u8>) -> usize {
+    let next_line_start = line_start + line.len();
+    data[line_start..next_line_start].copy_from_slice(line);
+    next_line_start
+}
+
+fn unfilter_sub(bpp: usize, line: &[u8], line_start: usize, data: &mut Vec<u8>) -> usize {
     let data_line = &mut data.as_mut_slice()[line_start..];
     line.iter().enumerate().for_each(|(i, p)| {
         let left = if i >= bpp { data_line[i - bpp] } else { 0 };
         data_line[i] = p.wrapping_add(left);
     });
-    line_start as isize
+    line_start + line.len()
+}
+
+fn unfilter_up(line: &[u8], line_start: usize, data: &mut Vec<u8>) -> usize {
+    if line_start == 0 {
+        line.iter().enumerate().for_each(|(i, p)| {
+            data[line_start + i] = *p;
+        });
+    } else {
+        let previous_line_start = line_start - line.len();
+        line.iter().enumerate().for_each(|(i, p)| {
+            let up = data[previous_line_start + i];
+            data[line_start + i] = p.wrapping_add(up);
+        });
+    }
+    line_start + line.len()
+}
+
+fn unfilter_average(bpp: usize, line: &[u8], line_start: usize, data: &mut Vec<u8>) -> usize {
+    if line_start == 0 {
+        line.iter().enumerate().for_each(|(i, p)| {
+            let left = if i >= bpp {
+                data[line_start + i - bpp]
+            } else {
+                0
+            };
+            data[line_start + i] = p.wrapping_add(left);
+        });
+    } else {
+        let previous_line_start = line_start - line.len();
+        line.iter().enumerate().for_each(|(i, p)| {
+            let up = data[previous_line_start + i] as u16;
+            let left = if i >= bpp {
+                data[line_start + i - bpp]
+            } else {
+                0
+            } as u16;
+            data[line_start + i] = p.wrapping_add(((up + left) / 2) as u8);
+        });
+    }
+    line_start + line.len()
+}
+
+fn unfilter_paeth(bpp: usize, line: &[u8], line_start: usize, data: &mut Vec<u8>) -> usize {
+    if line_start == 0 {
+        line.iter().enumerate().for_each(|(i, p)| {
+            let left = if i >= bpp {
+                data[line_start + i - bpp]
+            } else {
+                0
+            };
+            data[line_start + i] = p.wrapping_add(paeth_predictor(left, 0, 0));
+        });
+    } else {
+        let previous_line_start = line_start - line.len();
+        line.iter().enumerate().for_each(|(i, p)| {
+            let (up_left, up, left) = if i >= bpp {
+                (
+                    data[previous_line_start + i - bpp],
+                    data[previous_line_start + i],
+                    data[line_start + i - bpp],
+                )
+            } else {
+                (0, data[previous_line_start + i], 0)
+            };
+            data[line_start + i] = p.wrapping_add(paeth_predictor(left, up, up_left));
+        });
+    }
+    line_start + line.len()
+}
+
+// http://www.libpng.org/pub/png/spec/1.2/png-1.2-pdg.html#Filters
+// ; a = left, b = above, c = upper left
+// p := a + b - c        ; initial estimate
+// pa := abs(p - a)      ; distances to a, b, c
+// pb := abs(p - b)
+// pc := abs(p - c)
+// ; return nearest of a,b,c,
+// ; breaking ties in order a,b,c.
+// if pa <= pb AND pa <= pc then return a
+// else if pb <= pc then return b
+// else return c
+fn paeth_predictor(left: u8, up: u8, up_left: u8) -> u8 {
+    let (a, b, c) = (left as i16, up as i16, up_left as i16);
+    let p = a + b - c; // initial estimate
+    let pa = (p - a).abs();
+    let pb = (p - b).abs();
+    let pc = (p - c).abs();
+    if pa <= pb && pa <= pc {
+        left // a
+    } else if pb <= pc {
+        up // b
+    } else {
+        up_left // c
+    }
 }
 
 struct Img {
