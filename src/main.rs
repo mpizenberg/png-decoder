7ead3fd303d8159b8c4b470a12b274a26d367dbd
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2019-08-11 12:31:06 +0200

    Move decode_verbose to png module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use std::{env, error::Error, fs};
-
-// inner modules
-use png_decoder::chunk::{self, ChunkType};
-use png_decoder::chunk_data::{self, ChunkData};
-use png_decoder::filter::Filter;
 use png_decoder::png;
+use std::{env, error::Error, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,53 +12,7 @@ fn run(args: &[String]) -> Result<(), Box<Error>> {
     let data = fs::read(&args[1])?;
     let _ = png::decode_no_check_verbose_bis(&data)?;
     // let _ = png::decode_no_check_verbose(&data)?;
-    // let _ = decode_verbose(&data)?;
+    // let _ = png::decode_verbose(&data)?;
     println!("All done!");
     Ok(())
-}
-
-pub fn decode_verbose(data: &[u8]) -> Result<(), Box<Error>> {
-    match png::parse_chunks(data) {
-        Ok((_, chunks)) => {
-            let chunks_valid = chunk::validate_chunk_constraints(&chunks)?;
-            let idats: Vec<_> = chunks
-                .iter()
-                .filter(|c| c.chunk_type == ChunkType::IDAT)
-                .collect();
-            let inflated_idats = chunk_data::inflate_idats(idats.as_slice())?;
-            let ihdr_chunk = &chunks_valid[0];
-            let ihdr_data = chunk_data::parse_ihdr_data(ihdr_chunk.data).unwrap().1;
-            let scanlines = png::lines_slices(&inflated_idats, ihdr_data.scanline_width());
-            println!("Inflate image data size: {}", inflated_idats.len());
-            // println!("Scanlines:\n{:?}", scanlines);
-            display_filters(&scanlines);
-            let img = png::unfilter(&ihdr_data, scanlines);
-            println!("{:?}", img.get(77, 21));
-            println!("{:?}", img.get(78, 21));
-            println!("{:?}", img.get(79, 21));
-            println!("{:?}", img.get(80, 21));
-            println!("{:?}", img.get(81, 21));
-            println!("{:?}", img.get(82, 21));
-            // println!("{:?}", &img.data.as_slice()[0..10]);
-            chunks_valid.iter().for_each(|chunk| {
-                match chunk_data::parse_chunk_data(chunk) {
-                    Ok((_, ChunkData::Unknown(_))) => println!("{}", chunk),
-                    Ok((_, chunk_data)) => println!("{:?}", chunk_data),
-                    Err(e) => eprintln!("{:?}", e),
-                };
-            });
-        }
-        Err(e) => {
-            eprintln!("{:?}", e);
-        }
-    }
-    Ok(())
-}
-
-fn display_filters(scanlines: &[(Filter, &[u8])]) -> () {
-    scanlines
-        .iter()
-        .enumerate()
-        .for_each(|(i, (filter, _))| print!("{} {:?}, ", i, filter));
-    println!("");
 }
--- src/png.rs
+++ src/png.rs
@@ -5,8 +5,8 @@ use std::convert::TryFrom;
 use std::error::Error;
 
 // inner modules
-use crate::chunk::{Chunk, ChunkType};
-use crate::chunk_data::{self, IHDRData};
+use crate::chunk::{self, Chunk, ChunkType};
+use crate::chunk_data::{self, ChunkData, IHDRData};
 use crate::color::ColorType;
 use crate::filter::{self, Filter};
 
@@ -51,6 +51,43 @@ pub fn decode_no_check(input: &[u8]) -> Result<Png, Box<Error>> {
         }
         Err(e) => Err(format!("{:?}", e).into()),
     }
+}
+
+pub fn decode_verbose(data: &[u8]) -> Result<(), Box<Error>> {
+    match parse_chunks(data) {
+        Ok((_, chunks)) => {
+            let chunks_valid = chunk::validate_chunk_constraints(&chunks)?;
+            let idats: Vec<_> = chunks
+                .iter()
+                .filter(|c| c.chunk_type == ChunkType::IDAT)
+                .collect();
+            let inflated_idats = chunk_data::inflate_idats(idats.as_slice())?;
+            let ihdr_chunk = &chunks_valid[0];
+            let ihdr_data = chunk_data::parse_ihdr_data(ihdr_chunk.data).unwrap().1;
+            let scanlines = lines_slices(&inflated_idats, ihdr_data.scanline_width());
+            println!("Inflate image data size: {}", inflated_idats.len());
+            display_filters(&scanlines);
+            let img = unfilter(&ihdr_data, scanlines);
+            println!("{:?}", img.get(77, 21));
+            println!("{:?}", img.get(78, 21));
+            println!("{:?}", img.get(79, 21));
+            println!("{:?}", img.get(80, 21));
+            println!("{:?}", img.get(81, 21));
+            println!("{:?}", img.get(82, 21));
+            // println!("{:?}", &img.data.as_slice()[0..10]);
+            chunks_valid.iter().for_each(|chunk| {
+                match chunk_data::parse_chunk_data(chunk) {
+                    Ok((_, ChunkData::Unknown(_))) => println!("{}", chunk),
+                    Ok((_, chunk_data)) => println!("{:?}", chunk_data),
+                    Err(e) => eprintln!("{:?}", e),
+                };
+            });
+        }
+        Err(e) => {
+            eprintln!("{:?}", e);
+        }
+    }
+    Ok(())
 }
 
 pub fn decode_no_check_verbose(input: &[u8]) -> Result<Png, Box<Error>> {
@@ -207,4 +244,12 @@ pub fn lines_num(data: &[u8], scanline_width: usize) -> Vec<(Filter, usize)> {
             )
         })
         .collect()
+}
+
+fn display_filters(scanlines: &[(Filter, &[u8])]) -> () {
+    scanlines
+        .iter()
+        .enumerate()
+        .for_each(|(i, (filter, _))| print!("{} {:?}, ", i, filter));
+    println!("");
 }
